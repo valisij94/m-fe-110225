9158bc4d597b55c8a62f0cd12053d8b7e8a6617e
Davy <davyd.dabahian@cheetahdigital.com>
2025-05-06 12:08:43 +0300

    transitional results

--- lecture_16_promises_fetch/scripts/main.js
+++ lecture_16_promises_fetch/scripts/main.js
@@ -2,12 +2,12 @@ function renderProduct(product){
   const productCard = document.createElement('div');
   productCard.classList.add('productCard');
 
-  productCard.addEventListener('click', (event) => {
+  productCard.addEventListener('click', () => {
       productCard.classList.toggle('productCardHighlighted');
   });
 
   const productTitle = document.createElement('h3');
-  productTitle.textContent = product.name;
+  productTitle.textContent = product.title;
   productTitle.classList.add('productTitle');
 
   const productPrice = document.createElement('p');
@@ -19,7 +19,7 @@ function renderProduct(product){
   productCategory.classList.add('productCategory');
 
   const productImg = document.createElement('img');
-  productImg.src = !!product.img ? product.img : '';
+  productImg.src = !!product.images[0] ? product.images[0] : '';
   productImg.classList.add('productImg');
 
   const productDescription = document.createElement('p');
@@ -30,3 +30,63 @@ function renderProduct(product){
 
   return productCard;
 }
+
+const productsContainerEl = document.querySelector('.productsContainer');
+const selectEl = document.createElement('select');
+
+const loaderEl = document.createElement('div');
+loaderEl.classList.add('loader');
+loaderEl.textContent = 'Please wait!';
+
+productsContainerEl.before(selectEl);
+
+selectEl.before(loaderEl);
+loaderEl.style.display = 'none';
+
+fetch('https://dummyjson.com/products/categories')
+  .then( response => {
+    if (!response.ok) {
+      console.log('Something went wrong! HTTP:' + response.status );
+    } else {
+      return response.json();
+    }
+  })
+  .then( data => {
+    const placeholderOptionEl = document.createElement('option');
+    placeholderOptionEl.disabled = 'true';
+    placeholderOptionEl.selected = 'true';
+    placeholderOptionEl.textContent = 'Select something';
+    selectEl.append(placeholderOptionEl);
+    data.forEach( (category) => {
+      const optionEl = document.createElement('option');
+      optionEl.textContent = category.name;
+      optionEl.value = category.slug;
+      selectEl.append(optionEl);
+    });
+  });
+
+function getProducts(categoryName) {
+  let url = `https://dummyjson.com/products`;
+  if (!!categoryName) {
+    url += `/category/${categoryName}`;
+  }
+  loaderEl.style.display = 'block';
+  fetch(url)
+    .then( response => response.json() )
+    .then( data => {
+      productsContainerEl.innerHTML = '';
+      data.products.forEach( product => {
+        const productEl = renderProduct(product);
+        productsContainerEl.append(productEl);
+      });
+    })
+    .finally( () => {
+      loaderEl.style.display = 'none';
+    });
+}
+
+getProducts()
+
+selectEl.addEventListener( 'change', (event) => {
+  getProducts(event.target.value);
+});
