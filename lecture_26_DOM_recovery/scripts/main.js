0a5432b4d3b352500b9f5381f8e6e9e9f511cb0a
Davy <davyd.dabahian@cheetahdigital.com>
2025-05-28 12:53:13 +0300

    final result

--- lecture_26_DOM_recovery/scripts/main.js
+++ lecture_26_DOM_recovery/scripts/main.js
@@ -12,6 +12,8 @@ class Slider {
   #imagesContainerEl;
   // ссылка на ДОМ-элемент контейнера точек навигации
   #dotsContainerEl;
+  // ссылка на интервал автопрокрутки
+  #autoPlayInterval;
 
   constructor(selector, imagesArray) {
     const parent = document.querySelector(selector);
@@ -48,6 +50,10 @@ class Slider {
 
   #moveNext() {
     this.#goToSlide(this.#currentImgIndex + 1)
+  }
+
+  #startAutoPlay() {
+    this.#autoPlayInterval = setInterval(this.#moveNext.bind(this), 2000);
   }
 
   #init() {
@@ -106,10 +112,18 @@ class Slider {
     btnNext.addEventListener('click', this.#moveNext.bind(this));
     navContainerEl.append(btnPrev, btnNext);
     // собираем все воедино
-    sliderViewEl.append(imagesContainerEl);
-    sliderContainerEl.append(navContainerEl, sliderViewEl, dotsContainer);
+    sliderViewEl.append(navContainerEl, imagesContainerEl);
+    sliderContainerEl.append(sliderViewEl, dotsContainer);
     // добавляем контейнер слайдера в родительский ДОМ-узел
     this.#rootNode.append(sliderContainerEl);
+    // запускаем автопрокрутку
+    this.#startAutoPlay();
+    this.#rootNode.addEventListener('mouseover', () => {
+      clearInterval(this.#autoPlayInterval);
+    });
+    this.#rootNode.addEventListener('mouseout', () => {
+      this.#startAutoPlay();
+    });
   }
 }
 
@@ -120,6 +134,7 @@ const dataSource = [
   'https://images.prom.ua/1029691327_w600_h600_labrador-retriver.jpg',
   'https://www.happypet.care/_next/image?url=https%3A%2F%2Fhappypetproduction.s3.ap-south-1.amazonaws.com%2FbreedFiles%2Fdog%2F64d6461c7712a1493d6bc279%2FgalleryImages%2Flarge_file_1725051198278.webp&w=1080&q=75',
   'https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=532,height=532,fit=cover/animal/breed/dog/adult/66fd0ed8598bc632838841.jpg'];
+
 const sliderEl = new Slider('.dogsSlider', dataSource);
 
 /**
